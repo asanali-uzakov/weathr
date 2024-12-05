1909b8948a43952ba61641185107c5089d7e04b1
Assanali Uzakov <assanali.uzakov@interhome.group>
2024-12-05 14:17:01 +0100

    created weather store

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    {{ weatherData?.current }}
   </div>
 </template>
+
+<script setup lang="ts">
+useWeatherStore().load()
+const { weatherData } = storeToRefs(useWeatherStore())
+</script>
+
+<style scoped>
+
+</style>
