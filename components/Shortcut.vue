51ea00f17e73de1476d4b178593e996afd5e9918
Assanali Uzakov <assanali.uzakov@interhome.group>
2024-12-11 18:55:49 +0100

    fixed linting

--- components/Shortcut.vue
+++ components/Shortcut.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <kbd class="pointer-events-none inline-flex select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground">
     <slot />
