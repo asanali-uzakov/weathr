bc14555bd93e9da2a01eedb2b6ef612b5184a883
Assanali Uzakov <assanali.uzakov@interhome.group>
2024-12-11 18:27:54 +0100

    added error page

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-dvh w-full flex flex-col gap-4 justify-center items-center">
+    <Icon
+      name="line-md:alert-circle-loop"
+      class="size-32 text-muted-foreground"
+    />
+    <h1 class="text-4xl font-bold">
+      Oops!
+    </h1>
+    <p class="font-medium text-lg">
+      Something went wrong : (
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
